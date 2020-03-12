9d578ae0c935c5bce7d1b6b201c311be691b2698
jiehuhuhuhu <jiehuhuhuhu@gmail.com>
2020-03-12 15:18:18 +0800

    JSONHandle: Add post when DOM loaded

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-function readTextFile(callback) {
+var setupBool = false;
+
+function readJSONFile(callback) {
     var rawFile = new XMLHttpRequest();
     rawFile.overrideMimeType("application/json");
     rawFile.open("GET", "data.json", true);
@@ -12,8 +14,27 @@ function readTextFile(callback) {
 
 function init() {
   console.log("init");
-  readTextFile(function(text) {
+  readJSONFile(function(text) {
     var data = JSON.parse(text);
-    console.log(data);
+    createPost(data);
   });
 }
+
+function createPost(data) {
+  for(let i = 0; i < data.posts.length; i++) {
+    console.log(data.posts[i].text);
+
+    var p = document.createElement('p');
+    p.innerHTML = data.posts[i].text;
+    document.getElementById("posts").appendChild(p);
+  }
+}
+
+// initial setup
+document.addEventListener('DOMContentLoaded', (event) => {
+  console.log('DOM Loaded')
+  if (!setupBool) {
+    init();
+    setupBool = true;
+  }
+});
